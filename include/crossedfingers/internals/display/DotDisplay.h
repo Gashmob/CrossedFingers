fb4395e53c48bde3f4f46e22df9ab4de060fa0f7
Kevin Traini <kevin@ktraini.com>
2025-07-31 22:13:50 +0200

    feat: Add dot display
    Closes #41
    
    It prints less details
    
    - . for each passed case
    - S for skipped
    - W in yellow for warning
    - F with red background for failures
    
    The summary is count of passed and failed + details on warning and
    failures

---
+++ include/crossedfingers/internals/display/DotDisplay.h
@@ -0,0 +1,58 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2025-Present Kevin Traini
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef DOTDISPLAY_H
+#define DOTDISPLAY_H
+
+#include "Display.h"
+
+namespace crossedfingers::internals {
+class DotDisplay final : public Display {
+  public:
+    auto printBeginSuite(const std::string &suite_name) -> void override;
+
+    auto printBeginCase(const std::string &case_name) -> void override;
+
+    auto printEndCase(const std::string &case_name) -> void override;
+
+    auto printEndSuite(const std::string &suite_name) -> void override;
+
+    auto printSkipCase(const std::string &case_name) -> void override;
+
+    auto printWarningCase(const std::string &case_name) -> void override;
+
+    auto printFailCase(const std::string &case_name, const std::string &message) -> void override;
+
+    auto printSummary(
+        int test_count,
+        int assertion_count,
+        const std::vector<std::string> &succeed_tests,
+        const std::vector<std::string> &skipped_tests,
+        const std::map<std::string, std::string> &warning_tests,
+        const std::map<std::string, std::string> &failed_tests
+    ) -> void override;
+};
+} // namespace crossedfingers::internals
+
+#endif // DOTDISPLAY_H
